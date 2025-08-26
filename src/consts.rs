48f8bd47c5b10a6d3edfe2448694f4c0c7c9f4a5
splurf <ejsch03@gmail.com>
2025-08-25 21:27:48 -0400

    api change

--- src/consts.rs
+++ src/consts.rs
@@ -1,4 +1,1 @@
-use solana_sdk::pubkey::Pubkey;
-
-pub const JUP_API: &str = "https://lite-api.jup.ag/swap/v1";
-pub const WSOL: Pubkey = Pubkey::from_str_const("So11111111111111111111111111111111111111112");
+pub const JUP_API: &str = "https://api.jup.ag/ultra/v1";
